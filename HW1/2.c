11454f6e2c55ceeb39f2b7a9fb3756085256384f
Oscarshiah <t113318102@ntut.org.tw>
2025-02-18 11:49:08 +0800

    Create 2.c

---
+++ HW1/2.c
@@ -0,0 +1,13 @@
+void moveZeroes(int* nums, int numsSize) {
+    int k[numsSize];
+    int index=0;
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]!=0)
+        {
+            nums[index++]=nums[i];
+        }
+    }
+    for(int i=index;i<numsSize;i++){
+        nums[i]=0;
+    }
+}
